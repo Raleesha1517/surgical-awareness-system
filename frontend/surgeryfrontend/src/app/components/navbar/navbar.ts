ef560f31dd3fbbc1d1a3bb8d58c2428e9811fafa
Raleesha Niwanthaka <ralii1999430@gmail.com>
2025-12-23 14:09:04 +0530

    Navbar changed added language button

--- frontend/surgeryfrontend/src/app/components/navbar/navbar.ts
+++ frontend/surgeryfrontend/src/app/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
