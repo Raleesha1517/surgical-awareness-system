239af41f79f0a25811f256f87c7bc65604eb314b
Raleesha Niwanthaka <ralii1999430@gmail.com>
2025-12-26 20:34:43 +0530

    Language propup added

--- frontend/surgeryfrontend/src/app/app.ts
+++ frontend/surgeryfrontend/src/app/app.ts
@@ -3,20 +3,35 @@ import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Navbar } from "./components/navbar/navbar";
 import { Footer } from "./components/footer/footer";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']   // <-- fixed typo, was styleUrl
 })
 export class App{
   protected readonly title = signal('surgeryfrontend');
 
-  constructor(private translate: TranslateService) {}
+  showLangPopup = false; // 🔥 controls popup
+
+  constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem('lang');
+
+    if (savedLang) {
+      // If user already selected language before
+      this.translate.use(savedLang);
+    } else {
+      // First time opening app
+      this.showLangPopup = true;
+    }
+  }
 
   changeLang(lang: string) {
-    this.translate.use(lang); // 🔥 real-time change
+    this.translate.use(lang);           // change language instantly
+    localStorage.setItem('lang', lang); // save choice
+    this.showLangPopup = false;          // close popup
   }
 
  
