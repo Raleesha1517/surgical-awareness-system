6f645431f2e1b3b505bf442b2fb1caf30c0a1988
Raleesha Niwanthaka <ralii1999430@gmail.com>
2026-01-08 21:20:16 +0530

    instruction page added

--- frontend/surgeryfrontend/src/app/components/instructions/instructions.ts
+++ frontend/surgeryfrontend/src/app/components/instructions/instructions.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-instructions',
-  imports: [],
+  imports: [TranslatePipe, RouterLink, RouterLinkActive],
   templateUrl: './instructions.html',
   styleUrl: './instructions.scss',
 })
 export class Instructions {
+constructor(private translate: TranslateService) {}
 
+  changeLang(lang: string) {
+    this.translate.use(lang); // 🔥 real-time change
+  }
 }
