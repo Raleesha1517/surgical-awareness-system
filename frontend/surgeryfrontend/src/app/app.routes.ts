b4cceb26e1c399f4da24df41e1b93d6b89b293ce
Raleesha Niwanthaka <ralii1999430@gmail.com>
2026-01-08 20:45:00 +0530

    new button for the home page 2

--- frontend/surgeryfrontend/src/app/app.routes.ts
+++ frontend/surgeryfrontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { Instructions } from './components/instructions/instructions';
 
 export const routes: Routes = [
         { path: '', component: Home },
+        { path: 'instructions', component: Instructions },
 ];
